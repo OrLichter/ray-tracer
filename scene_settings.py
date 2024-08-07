c166d2671b8e0626df57cf99e9e9736827e5801f
eyal gomel <eyalgomel12@gmail.com>
2024-08-07 15:49:07 +0300

    type fix

--- scene_settings.py
+++ scene_settings.py
@@ -4,5 +4,5 @@ import torch
 class SceneSettings:
     def __init__(self, background_color, root_number_shadow_rays, max_recursions):
         self.background_color = torch.tensor(background_color)
-        self.root_number_shadow_rays = root_number_shadow_rays
+        self.root_number_shadow_rays = int(root_number_shadow_rays)
         self.max_recursions = max_recursions
